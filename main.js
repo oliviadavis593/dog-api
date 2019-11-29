dde41b36c4173fcc749249aaa28e25746dccb4f5
Olivia Davis <olivia.davis593@gmail.com>
2019-11-29 13:57:39 -0600

    made changes to files

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 'use strict';
 
-function getDogImage() {
-    const options = {method: 'GET'};
-    fetch('https://dog.ceo/api/breeds/image/random/3 Fetch!' , options)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
-    
+function getDogImages(numberInput) {
+    if(numberInput < 3) {
+        fetch('https://dog.ceo/api/breeds/image/random/3')
+            .then(response => response.json())
+            .then(responseJson => console.log(responseJson));
+    }
+    else if (numberInput > 50) {
+        return alert('Please choose a number that is equal to or less than 50');
+    }
+    else {
+        fetch('https://dog.ceo/api/breeds/image/random/${numberInput}')
+            .then(response => response.json())
+            .then(responseJson => console.log(responseJson));
+    }
 }
+
+function userInput() {
+    $('#dog-input-form').submit(e => {
+        e.preventDefault();
+        let numberInput = $('#number-dog').val();
+        getDogImages(userInput);
+    });
+}
+
+$(function() {
+    console.log('App is loading!');
+    userInput();
+})
+
 
